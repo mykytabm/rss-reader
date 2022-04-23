6c13a9f8c5f26123155a5ffab4ac29a54aca0642
nikitabm <nikitabalabuiev@gmail.com>
2022-04-23 18:48:39 +0200

    comment out  background worker

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ elif [ -f /app/main.py ]; then
     DEFAULT_MODULE_NAME=main
 fi
 MODULE_NAME=${MODULE_NAME:-$DEFAULT_MODULE_NAME}
-VARIABLE_NAME=${VARIABLE_NAME:-app}
+VARIABLE_NAME=${VARIABLE_NAME:-app} 
 export APP_MODULE=${APP_MODULE:-"$MODULE_NAME:$VARIABLE_NAME"}
 
 if [ -f /app/gunicorn_conf.py ]; then
@@ -29,8 +29,8 @@ if [ -f $PRE_START_PATH ] ; then
 else 
     echo "There is no script $PRE_START_PATH"
 fi
-#Start dramatiq workers in background
-dramatiq services --path ./app/ &
+# #Start dramatiq workers in background
+# dramatiq services --path ./app/ &
 
 # Start Gunicorn
 exec gunicorn -k "$WORKER_CLASS" -c "$GUNICORN_CONF" "$APP_MODULE"
